6bfbd938c408b9d6a4a51da780008c0477401fd2
GloriahMoses <mosesgloriah96@gmail.com>
2019-12-11 23:50:24 +0300

    Update Main.java

--- cat2/Main.java
+++ cat2/Main.java
@@ -1,3 +1,10 @@
+/**
+ *<h1>Booking System</h1>
+ *This program allows uses to Book Hotel Rooms
+ * *This program allows uses to Book Hotel Rooms
+ * Details are saved to a database
+ */
+
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
@@ -15,11 +22,20 @@ import java.sql.*;
 
 public class Main extends Application implements Interface{
 
+    /**
+     * Builds the Scene
+     *
+     *@param	Stage	Needs empty stage to set scene in
+     *@return 	void	none
+     */
+
+    //Connect to the database
     Connection conn = null;
     String DB_URL = "jdbc:mysql://localhost:3306/bksystem";
     String USER = "root";
     String  PASS = "";
 
+    //Create Stage, scene
     Stage window;
     Scene scene, scene1;
 
@@ -44,7 +60,11 @@ public class Main extends Application implements Interface{
         gridpane.setHgap(10);
         gridpane.setAlignment(Pos.CENTER);
 
+        //BackgroundImage
+
+        //add gridpane to the scene
         scene = new Scene(gridpane, 1024, 600);
+
 
         //Creating a Text object
         Text text = new Text("Welcome Our Booking System");
@@ -58,7 +78,8 @@ public class Main extends Application implements Interface{
         gridpane.add(clickbook, 0, 4);
         clickbook.setOnAction(e -> window.setScene(scene1));
 
-        //Scene2
+
+        //Scene2, Screen2
         GridPane gridpane1 = new GridPane();
         gridpane1.setPadding(new Insets(10, 10, 10, 10));
         gridpane1.setVgap(10);
@@ -66,13 +87,16 @@ public class Main extends Application implements Interface{
         gridpane1.setAlignment(Pos.CENTER);
         scene1 = new Scene(gridpane1, 1024, 600);
 
+        //Creating a Text object
         Text text2= new Text("You are in the system");
         gridpane1.add(text2, 0, 0);
 
+        //Creating checkin field
         DatePicker checkin = new DatePicker();
         checkin.setPromptText("Check In");
         gridpane1.add(checkin, 0, 6);
 
+        //disable previous dates
         checkin.setDayCellFactory(picker -> new DateCell() {
             public void updateItem(LocalDate date, boolean empty) {
                 super.updateItem(date, empty);
@@ -81,10 +105,12 @@ public class Main extends Application implements Interface{
             }
         });
 
+        //Creating checkout field
         DatePicker checkout = new DatePicker();
         checkout.setPromptText("Check Out");
         gridpane1.add(checkout, 4, 6);
 
+        //disable previous dates
         checkout.setDayCellFactory(picker -> new DateCell() {
             public void updateItem(LocalDate date, boolean empty) {
                 super.updateItem(date, empty);
@@ -93,8 +119,8 @@ public class Main extends Application implements Interface{
             }
         });
 
+        //Create a dropdown for rooms
         ChoiceBox<String> room= new ChoiceBox<String>();
-        //ChoiceBox.setPromptText("Select Room");
         room.getItems().addAll("Standard Room", "Deluxe Room", "Superior Room", "Junior Suite", "Presidential Suit");
         gridpane1.add(room, 0, 8);
 
@@ -104,6 +130,7 @@ public class Main extends Application implements Interface{
         emailField.setPromptText("Email");
         gridpane1.add(emailField, 4, 8);
 
+        //Create submit button, add values to db
         Button submit = new Button("Submit Here");
         gridpane1.add(submit, 10, 7);
 
@@ -119,21 +146,20 @@ public class Main extends Application implements Interface{
                     stmt.setDate(2, Date.valueOf(checkout.getValue()));
                     stmt.setString(3, (String) room.getValue());
                     stmt.setString(4,emailField.getText());
-			
-			checkin.getEditor().clear();
+
+                    checkin.getEditor().clear();
                     checkout.getEditor().clear();
                     room.getSelectionModel().clearSelection();
                     emailField.clear();
-			
+
                     stmt.executeUpdate();}catch(SQLException se){
                     se.printStackTrace();
-                    
+
                 }
             }
         };
 
         submit.addEventHandler(MouseEvent.MOUSE_CLICKED,eventHandler);
-
 
         //Setting title to the Stage
         stage.setTitle("Booking System");
@@ -145,12 +171,9 @@ public class Main extends Application implements Interface{
         stage.show();
     }
 
-    /*public static void main(String args[]){
-        launch(args); }
-	*/
-	
-	@Override
-	public void invoke(){
-		launch();
-	}
+    @Override
+    public void invoke(){
+        launch();
+    }
+
 }
